3e08dd923aecea4ca09cce521bcb30b591161018
Asish Kumar <asish.rayagada@gmail.com>
2025-12-16 19:37:00 +0530

    "indentation fix"

--- src/app.js
+++ src/app.js
@@ -4,14 +4,13 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const { connectDB } = require('./config/database')
 
-
 const app = express()
-// app.use(
-//   cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true
-//   })
-// )
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  })
+)
 
 app.use(express.json())
 app.use(cookieParser())
@@ -19,13 +18,12 @@ const authRouter = require('./routes/auth')
 const clientRouter = require('./routes/clientRoutes')
 const taskRouter = require('./routes/taskRoutes')
 
-
 //login and signup auth
 app.use('/auth', authRouter)
 //Clients auth
 app.use('/api/clients', clientRouter)
 //task routes
- app.use('/api/tasks' , taskRouter)
+app.use('/api/tasks', taskRouter)
 app.use('/', require('./routes/testSwagger'))
 if (process.env.NODE_ENV == 'production') {
   const swaggerUI = require('swagger-ui-express')
@@ -34,14 +32,14 @@ if (process.env.NODE_ENV == 'production') {
   //console.log(swaggerSpec)
   app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 }
+//check health of db and deployed model
 const healthCheck = require('./routes/health')
-app.use('/' , healthCheck)
+app.use('/', healthCheck)
 connectDB()
   .then(() => {
     console.log('connection established with database')
     app.listen(process.env.PORT, () => {
-        console.log('listening to port')
-        
+      console.log('listening to port')
     })
   })
   .catch(error => {
